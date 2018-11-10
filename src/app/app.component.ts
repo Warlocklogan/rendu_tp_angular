a42aeb6241ab8dc4aae3bcdc45fdb36085109a55
Logan Gonet <logan.gonet@hotmail.fr>
2018-11-10 10:09:22 +0100

    Finish blog exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from '../app/post';
 
 @Component({
   selector: 'app-root',
@@ -6,20 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts = [
+  posts = Array<Post>(
     {
       title: 'Mon premier post',
-      text: 'lorem ipsum',
+      content: 'lorem ipsum',
+      loveIts: 1,
+      created_at: new Date(),
     },
     {
       title: 'Mon deuxième post',
-      text: 'lorem ipsum',
+      content: 'lorem ipsum',
+      loveIts: -1,
+      created_at: new Date(),
     },
     {
       title: 'Encore un post',
-      text: 'lorem ipsum',
-    },
-  ];
+      content: 'lorem ipsum',
+      loveIts: 0,
+      created_at: new Date(),
+    }
+  );
 
   constructor() {  }
 }
--- src/app/post-list-item-component/post-list-item-component.component.ts
+++ src/app/post-list-item-component/post-list-item-component.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-post-list-item-component',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListItemComponentComponent implements OnInit {
 
+  @Input() post: Post;
+  localLoveIt: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.localLoveIt = this.post.loveIts;
+  }
+
+  addLike(): void {
+    this.post.loveIts++;
+    this.localLoveIt++;
+  }
+
+  removeLike(): void {
+    this.post.loveIts--;
+    this.localLoveIt--;
   }
 
 }
